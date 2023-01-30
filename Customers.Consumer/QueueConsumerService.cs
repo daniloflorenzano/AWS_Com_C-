088e56346ac1b9ed85e94d9c166d40d0a4d335f2
daniloflorenzano <danilo@directdigital.com.br>
2023-01-30 11:34:03 -0300

    Cria servico de consumo de mensagens e utiliaz mediator pattern

---
+++ Customers.Consumer/QueueConsumerService.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Customers.Consumer.Messages;
+using MediatR;
+using Microsoft.Extensions.Options;
+
+namespace Customers.Consumer;
+
+public class QueueConsumerService : BackgroundService
+{
+    private readonly IAmazonSQS _sqs;
+    private readonly IOptions<QueueSettings> _queueSettings;
+    private readonly IMediator _mediator;
+    private readonly ILogger<QueueConsumerService> _logger;
+
+    public QueueConsumerService(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings, IMediator mediator, ILogger<QueueConsumerService> logger)
+    {
+        _sqs = sqs;
+        _queueSettings = queueSettings;
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var queueUrlResponse = await _sqs.GetQueueUrlAsync(_queueSettings.Value.Name, stoppingToken);
+
+        var recieveMessageRequest = new ReceiveMessageRequest
+        {
+            QueueUrl = queueUrlResponse.QueueUrl,
+            AttributeNames = new List<string> { "All"},
+            MessageAttributeNames = new List<string> { "All" }
+        };
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var response = await _sqs.ReceiveMessageAsync(recieveMessageRequest, stoppingToken);
+            foreach (var message in response.Messages)
+            {
+                var messageType = message.MessageAttributes["MessageType"].StringValue;
+                var type = Type.GetType($"Customers.Consumer.Messages.{messageType}");
+
+                if (type is null)
+                {
+                    _logger.LogWarning("Tipo de mensagem desconhecido: {MessageType}", messageType);
+                    continue;
+                }
+
+                var typedMessage = (ISqsMessage)JsonSerializer.Deserialize(message.Body, type)!;
+
+                try
+                {
+                    await _mediator.Send(typedMessage, stoppingToken);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Mensagem falhou durante processo");
+                    continue;
+                }
+
+                await _sqs.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, stoppingToken);
+            }
+            
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
